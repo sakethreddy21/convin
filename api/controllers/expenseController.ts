776b41259df275b702eb814cbc4f40e21872165e
saketh <saketh.reddy@trainingmug.com>
2024-10-20 15:21:47 +0530

    fix : deployment fix

--- api/controllers/expenseController.ts
+++ api/controllers/expenseController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import Expense from '../models/Expense';
-import User from '../models/user';
+import User from '../models/User';
 import { Parser } from 'json2csv';  
 export const addExpense = async (req: Request, res: Response) => {
   const { title, amount, userEmail, participants, splitMethod, specificAmounts, percentages } = req.body;
--- api/controllers/userController.ts
+++ api/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import User from '../models/user';
+import User from '../models/User';
 import jwt from 'jsonwebtoken'; // Import jsonwebtoken
 import Joi from 'joi'; // Import Joi for validation
 
