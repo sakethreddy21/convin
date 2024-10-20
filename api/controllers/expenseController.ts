e8e5bb7a0a9418ee3f34dacb4c393862b27c0737
saketh <saketh.reddy@trainingmug.com>
2024-10-20 15:18:37 +0530

    fix : deployment fix

--- api/controllers/expenseController.ts
+++ api/controllers/expenseController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import Expense from '../models/expense';
+import Expense from '../models/Expense';
 import User from '../models/user';
 import { Parser } from 'json2csv';  
 export const addExpense = async (req: Request, res: Response) => {
